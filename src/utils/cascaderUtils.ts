54b12601075b6f9518f3d4fd719a612a57f60483
liaoyf <liaoy@sunsharing.com.cn>
2021-06-29 15:28:50 +0800

    feat: 处理依赖错误

--- src/utils/cascaderUtils.ts
+++ src/utils/cascaderUtils.ts
@@ -1,94 +1,113 @@
 import memoize from 'lodash.memoize';
-import {CascadeOptions} from "@/useCascade";
+import { CascadeOptions } from '@/useCascade';
 
 type isChildren = (item: CascadeOptions, subItem: CascadeOptions) => boolean;
 type isRoot = (item: CascadeOptions) => boolean;
 interface transListToTreeProps {
-  data: CascadeOptions[],
-  isChildren: isChildren,
-  isRoot: isRoot
+  data: CascadeOptions[];
+  isChildren: isChildren;
+  isRoot: isRoot;
+  valueKey: string;
 }
 
-const getChildrenNodes = (data: CascadeOptions[], item: CascadeOptions, isChildren: isChildren) => {
-    const findChildren = data.filter(
-        (subItem: CascadeOptions) => subItem.value !== item.value && isChildren(item, subItem)
+const getChildrenNodes = (
+  data: CascadeOptions[],
+  item: CascadeOptions,
+  isChildren: isChildren,
+  valueKey: string,
+) => {
+  const findChildren = data.filter(
+    (subItem: CascadeOptions) =>
+      subItem[valueKey] !== item[valueKey] && isChildren(item, subItem),
+  );
+
+  return findChildren.map((subItem: CascadeOptions) => {
+    const subItemData = {
+      ...subItem,
+    };
+    const subItemChildren = getChildrenNodes(
+      data,
+      subItem,
+      isChildren,
+      valueKey,
     );
+    if (subItemChildren.length > 0) {
+      subItemData.children = subItemChildren;
+    }
 
-    return findChildren.map((subItem: CascadeOptions) => {
-        const subItemData = {
-            ...subItem
-        };
-        const subItemChildren = getChildrenNodes(data, subItem, isChildren);
-        if (subItemChildren.length > 0) {
-            subItemData.children = subItemChildren;
-        }
-
-        return subItemData;
-    });
+    return subItemData;
+  });
 };
 
 // 处理平级数据 =》 嵌套结构, getChildren(item, childrenItem) => true
-export const transListToTree = ({ data = [], isChildren, isRoot }: transListToTreeProps) => {
-    let dealData = [];
+export const transListToTree = ({
+  data = [],
+  isChildren,
+  isRoot,
+  valueKey = 'value',
+}: transListToTreeProps) => {
+  let dealData = [];
 
-    dealData = data.filter(item => isRoot(item)).map(item => {
-        const children = getChildrenNodes(data, item, isChildren);
-        if (children) {
-            return {
-                ...item,
-                children
-            };
-        }
+  dealData = data
+    .filter((item) => isRoot(item))
+    .map((item) => {
+      const children = getChildrenNodes(data, item, isChildren, valueKey);
+      if (children) {
+        return {
+          ...item,
+          children,
+        };
+      }
     });
 
-    return dealData;
+  return dealData;
 };
 
 export const getLen = (options: CascadeOptions[]) => {
-    let maxLevel = 0;
+  let maxLevel = 0;
 
-    const mapOptions = (data: CascadeOptions[], level: number) => {
-        if (level > maxLevel) {
-            maxLevel = level;
-        }
-        data.forEach(option => {
-            if (option.children) {
-                mapOptions(option.children, level + 1);
-            }
-        });
-    };
+  const mapOptions = (data: CascadeOptions[], level: number) => {
+    if (level > maxLevel) {
+      maxLevel = level;
+    }
+    data.forEach((option) => {
+      if (option.children) {
+        mapOptions(option.children, level + 1);
+      }
+    });
+  };
 
-    mapOptions(options, 1);
+  mapOptions(options, 1);
 
-    return maxLevel;
+  return maxLevel;
 };
 
 // 获取 id map、pid map
 const memoizedNodes = memoize((data: CascadeOptions[], valueKey: string) => {
-    // 先把每级映射出来，然后在查找，不然每次都递归影响性能
-    const idMap: Record<string, any> = {};
-    const pidMap: Record<string, any> = {};
+  // 先把每级映射出来，然后在查找，不然每次都递归影响性能
+  const idMap: Record<string, any> = {};
+  const pidMap: Record<string, any> = {};
 
-    const mapOptions = (data: CascadeOptions[], parentIds: string[]) => {
-        data.map(item => {
-            const value = item[valueKey];
-            idMap[value] = item;
-            if (!pidMap[value]) {
-                pidMap[value] = [];
-            }
-            pidMap[value] = parentIds;
-            if (item.children) {
-                mapOptions(item.children, [...parentIds, value]);
-            }
-        });
-    };
+  const mapOptions = (data: CascadeOptions[], parentIds: string[]) => {
+    data.map((item) => {
+      const value = item[valueKey];
+      idMap[value] = item;
+      if (!pidMap[value]) {
+        pidMap[value] = [];
+      }
+      pidMap[value] = parentIds;
+      if (item.children) {
+        mapOptions(item.children, [...parentIds, value]);
+      }
+    });
+  };
 
-    mapOptions(data, []);
+  mapOptions(data, []);
 
-    return {
-        idMap,
-        pidMap
-    };
+  return {
+    idMap,
+    pidMap,
+  };
 });
 
 /**
@@ -98,10 +117,17 @@ const memoizedNodes = memoize((data: CascadeOptions[], valueKey: string) => {
  * @param valueKey
  * @returns {[]}
  */
-export const getSelectedOptions = (options: CascadeOptions[], value: string | number, valueKey: string) => {
-    const { idMap, pidMap } = memoizedNodes(options, valueKey);
+export const getSelectedOptions = (
+  options: CascadeOptions[],
+  value: string | number,
+  valueKey: string,
+) => {
+  const { idMap, pidMap } = memoizedNodes(options, valueKey);
 
-    return value
-        ? [...pidMap[value].map((childId: string | number) => idMap[childId]), idMap[value]]
-        : [];
+  return value
+    ? [
+        ...pidMap[value].map((childId: string | number) => idMap[childId]),
+        idMap[value],
+      ]
+    : [];
 };
