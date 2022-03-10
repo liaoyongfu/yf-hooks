1ebbce90275c567980f06cc82e9638a05ee5a970
liaoyf <liaoy@sunsharing.com.cn>
2022-03-10 15:32:08 +0800

    fix: useQuery 不解码 url

--- src/useQuery/index.ts
+++ src/useQuery/index.ts
@@ -4,10 +4,16 @@ import { parse } from 'qs';
 const useQuery = (location?: any) => {
   const locationResult = location || useLocation();
 
-  return parse(locationResult.search, { ignoreQueryPrefix: true }) as Record<
-    any,
-    any
-  >;
+  return parse(locationResult.search, {
+    ignoreQueryPrefix: true,
+    decoder(str, defaultDecoder, charset, type) {
+      if (type === 'key') {
+        return defaultDecoder(str); // Decoded key
+      } else if (type === 'value') {
+        return str; // Decoded value
+      }
+    },
+  }) as Record<any, any>;
 };
 
 export default useQuery;
